85e4221caf23e4961dd7147e049274a3af6ebc70
tomas938 <tomasalbert9387182@gmail.com>
2021-09-14 11:29:02 +0200

    add styles with no active states

---
+++ main.js
@@ -0,0 +1,64 @@
+const daily = document.querySelector("#daily");
+const weekly = document.querySelector("#weekly");
+const monthly = document.querySelector("#monthly");
+getData();
+async function getData() {
+	const res = await fetch("data.json");
+	const data = await res.json();
+	displayData(data);
+}
+function displayData(data) {
+	data.forEach((item, index) => {
+		const main = document.querySelector("main");
+		const statistic = document.createElement("div");
+		statistic.classList.add("statistics");
+		function renderMarkup(item, hrs, week) {
+			statistic.innerHTML = `<div class="statistics__item statistics__item-${index}">
+					<div class="top"></div>
+					<div class="bottom">
+						<div class="dashboard-class">
+							<h3>${item.title}</h3>
+							<svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+								<path
+									d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+									fill="#BBC0FF"
+									fill-rule="evenodd"
+								/>
+							</svg>
+						</div>
+						<div class="dashboard-stats">
+							<span>${hrs}hrs</span>
+							<p>Last Week - ${week}hrs</p>
+						</div>
+					</div>
+					</div>`;
+		}
+		renderMarkup(
+			item,
+			item.timeframes.weekly.current,
+			item.timeframes.weekly.previous
+		);
+		daily.addEventListener("click", () => {
+			renderMarkup(
+				item,
+				item.timeframes.daily.current,
+				item.timeframes.daily.previous
+			);
+		});
+		weekly.addEventListener("click", () => {
+			renderMarkup(
+				item,
+				item.timeframes.weekly.current,
+				item.timeframes.weekly.previous
+			);
+		});
+		monthly.addEventListener("click", () => {
+			renderMarkup(
+				item,
+				item.timeframes.monthly.current,
+				item.timeframes.monthly.previous
+			);
+		});
+		main.appendChild(statistic);
+	});
+}
